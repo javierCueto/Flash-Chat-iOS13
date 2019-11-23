0c4fd13d6909ea07a2b3a82efa81938ab9b6e08a
Javier <javelie@outlook.com>
2019-11-23 13:38:17 -0700

    finish challenge

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by José Javier Cueto Mejía on 20/11/19.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender : String
+    let body : String 
+}
---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,28 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by José Javier Cueto Mejía on 21/11/19.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 10
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
